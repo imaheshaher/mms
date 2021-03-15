6998259f86a0f5729425df7a8665a6e55511f82e
imaheshaher <maheshaher035@gmail.com>
2021-03-15 23:13:44 +0530

    commit

--- mms/settings.py
+++ mms/settings.py
@@ -53,8 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'corsheaders.middleware.CorsMiddleware'
 ]
 CORS_ORIGIN_WHITELIST = [
      'http://localhost:3000'
