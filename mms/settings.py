121606855b121fcd358580249a31cd00c1488a6d
imaheshaher <maheshaher035@gmail.com>
2021-03-15 23:12:45 +0530

    middleware

--- mms/settings.py
+++ mms/settings.py
@@ -53,7 +53,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 CORS_ORIGIN_WHITELIST = [
      'http://localhost:3000'
