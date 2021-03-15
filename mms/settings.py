99ca4579ac5d3da778889e7d954e141a682abf6a
imaheshaher <maheshaher035@gmail.com>
2021-03-15 22:20:43 +0530

    changed

--- mms/settings.py
+++ mms/settings.py
@@ -128,10 +128,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'assets'),
-)
+if DEBUG:
+        STATICFILES_DIRS = [
+            os.path.join(BASE_DIR, 'static')
+       ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # REST_FRAMEWORK = {
 #      'DEFAULT_PERMISSION_CLASSES': (
 #          'rest_framework.permissions.IsAuthenticated',
